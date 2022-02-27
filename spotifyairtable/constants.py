c4e88b903872c4d8754605eb7f5254192cb4c253
Annie Thiessen <anniethiessen79@gmail.com>
2022-02-27 15:57:36 -0700

    Update constants

--- spotifyairtable/constants.py
+++ spotifyairtable/constants.py
@@ -1,6 +1,20 @@
 SPOTIFY_SAMPLE_URLS = {
     'artists': {
         'Birdy': '2WX2uTcsvV5OnS0inACecP',
-        'Led Zeppelin': '36QJpDe2go2KgaRleHCDTp'
+        'Led Zeppelin': '36QJpDe2go2KgaRleHCDTp',
+        'Logic': '4xRYI6VqpkE3UwrDrAZL8L'
+    },
+    'albums': {
+        'Under No Pressure (Deluxe)': '1Mk7Lei0Ra3hawezqmad5V'
+    },
+    'tracks': {
+        'Bounce': '1632iucJ0H2wpzj0byUQau?si=850b61c8ab264fb9'
+    },
+    'playlists': {
+        'Coding Mode': '37i9dQZF1DX5trt9i14X7j',
+        'Grizzly Bear': '4Q0PQ3oZwFpuVtWqy2JGjC'
+    },
+    'users': {
+        'Annie': '22mdkvj2vksbrfot6ynfyum2y'
     }
 }
